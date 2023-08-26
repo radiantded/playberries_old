507852b8fc607698a3fa5d828e31551f295ece4a
Enver <radiantded@gmail.com>
2023-08-26 13:10:30 +0300

    added cart_rate option

--- fsms.py
+++ fsms.py
@@ -3,10 +3,12 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 
 class CreateTaskFSM(StatesGroup):
     create = State()
-    
+
+
 class StartTaskFSM(StatesGroup):
     start = State()
     finish = State()
+
 
 class DeleteTaskFSM(StatesGroup):
     delete = State()
